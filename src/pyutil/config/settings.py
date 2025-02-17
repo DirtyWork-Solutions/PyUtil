523df109e2924dbf2766c2fd6b02c3913bc48fe8
aidan <aidankhogg@gmail.com>
2025-02-17 14:11:07 +0000

    boilerplate settings tweaks

--- src/pyutil/config/settings.py
+++ src/pyutil/config/settings.py
@@ -1,9 +1,13 @@
 import yaml
 import os
 
+from omegaconf import DictConfig
+
+
 class Settings:
     """Loads configuration settings from a YAML file or environment variables."""
-    def __init__(self, config_path="config.yaml"):
+
+    def __init__(self, config_path="config.yml"):
         self.config_path = config_path
         self._config = self._load_config()
 
@@ -18,5 +22,13 @@ class Settings:
         """Retrieves a configuration value."""
         return self._config.get(key, default)
 
+    @property
+    def all(self):
+        all_settings = {}
+        for key in self._config.keys():
+            all_settings.update(self._config.get(key))
+
+        return DictConfig(all_settings)
+
 # Global settings instance
-settings = Settings()
+settings = Settings().all
